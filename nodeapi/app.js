dee512ae7dd33825c572c9b66190b9aa54c67742
Juan Marcos <juanmarcosmarinperez@gmail.com>
2020-05-07 19:06:09 +0200

    File created: / - /users - otros - /hello

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -21,7 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/otros', express.static(path.join(__dirname, 'otros')));
 
+app.get('/hello', function (req, res) {
+  res.send('Hello You! - This is just an answer from app.js file');
+});
+  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- nodeapi/public/javascripts/test.js
+++ nodeapi/public/javascripts/test.js
@@ -1,4 +1,5 @@
 'use strict';
-console.log('test.js in public folder found');
 let d = new Date();
 console.log(`Today's date: ${d.toLocaleDateString()}`);
+console.log('test.js in public folder found & linked OK');
+
